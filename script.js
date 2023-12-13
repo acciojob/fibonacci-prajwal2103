ac9923f53afa8a620635da9e41e00d749409657f
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-13 20:46:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function fibonacci(num) {
 	let a=0
 	let b=1;
 	let c;
-	if(num==0){
+	if(num==1){
 		return a;
+	}
+	if(num==2){
+		return b;
 	}
 for (let index = 3; index <=num; index++) {
 	 c=a+b;
