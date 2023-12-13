1a79f764982eccb7edf93e5f7b7f4653dc1b178e
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-13 20:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function fibonacci(num) {
 	let a=0
 	let b=1;
-for (let index = 2; index <=50; index++) {
-	let c=a+b;
+	let c;
+for (let index = 2; index <=num; index++) {
+	 c=a+b;
 	a=b;
 	b=c;
 }
+	return c;
 	
 }
 
